dd65fdd11ea3caa9f4d867ebb2590f5d95f37b27
Niklas <niklas.wretblad@combient.com>
2023-09-25 12:00:23 +0200

    Started writing db class

--- src/database_handler.py
+++
@@ -1,5 +0,0 @@
-
-
-class DatabaseHandler:
-    pass
-
---
+++ src/main.py
@@ -0,0 +1,8 @@
+
+from database_handler import DatabaseHandler
+
+def main():
+    db_handler = DatabaseHandler()
+
+if __name__ == "__main__":
+    main()
