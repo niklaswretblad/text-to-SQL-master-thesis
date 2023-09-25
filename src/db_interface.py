3bd75bfccdf5d44742b4d52558493c20718aa7fc
Niklas <niklas.wretblad@combient.com>
2023-09-25 15:34:03 +0200

    First db interface done

---
+++ src/db_interface.py
@@ -0,0 +1,64 @@
+
+import sqlite3
+import pandas as pd
+import json
+import os
+import time
+
+DB_BASE_PATH = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'data/db/'))
+
+class DBLoader:
+   accepted_databases = []
+   current_db = ""
+   conn = None
+
+   def __init__(self, accepted_databases):
+      self.accepted_databases = accepted_databases
+
+   def execute_query(self, sql, gold_sql, db_id):
+      db_path = DB_BASE_PATH + '/' + db_id + '/' + db_id + '.sqlite'
+
+      if self.current_db != db_id:
+         self.conn = sqlite3.connect(db_path)
+         self.cursor = self.conn.cursor()
+         self.current_db = db_id
+      
+      self.cursor.execute(sql)
+      pred_res = self.cursor.fetchall()
+
+      self.cursor.execute(gold_sql)
+      golden_res = self.cursor.fetchall()
+      
+      equal = (set(pred_res) == set(golden_res))
+      if equal:
+         return 1
+      else:
+         return 0
+   
+def load_json(path):
+   with open(path, 'r') as j:
+      data = json.loads(j.read())
+   return data
+
+def list_tables_and_columns(database_path):
+    conn = sqlite3.connect(database_path)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = cursor.fetchall()
+
+    for table in tables:
+        table_name = table[0]
+        print(f"Table: {table_name}")
+        
+        cursor.execute(f"PRAGMA table_info({table_name});")
+        columns = cursor.fetchall()
+        for column in columns:
+            col_name = column[1]
+            col_type = column[2]
+            print(f"  Column: {col_name}, Type: {col_type}")
+
+    conn.close()
+
+
+    
--- src/main.py
+++ src/main.py
@@ -1,9 +1,35 @@
 
-from bird_interface import BIRDInterface
+from db_interface import *
+
+QUESTIONS_PATH = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'data/questions.json'))
+
+
+ACCEPTED_DATABASES = [
+    'car_retails',
+    'financial',
+    # 'retail_world',
+    'retails'
+]
 
 def main():
-    bird_interface = BIRDInterface()
+    questions = load_json(QUESTIONS_PATH)
+    questions = [question for question in questions if question['db_id'] in ACCEPTED_DATABASES]
+    db_loader = DBLoader(ACCEPTED_DATABASES)
 
+    score = 0
+    total_questions = len(questions)
+    for i, row in enumerate(questions):
+        if row['db_id'] in ACCEPTED_DATABASES:
+            golden_sql = row['SQL']
+            db_id = row['db_id']
+            
+            res = db_loader.execute_query(golden_sql, golden_sql, db_id)
+            score += res
 
+            print("Percentage done: ", round(i / total_questions * 100, 2), "% Domain: ", db_id)
+
+    #list_tables_and_columns(DB_BASE_PATH + '/' + 'retail_world' + '/' + 'retail_world' + '.sqlite')
+
+    #print("accuracy: ", score / len(questions))
 if __name__ == "__main__":
     main()
