23b600fe1e631f66d4f0771b6064aae5a89b1db7
Fredrik <fredrik@Admins-MacBook-Pro-2.local>
2023-09-25 16:45:06 +0200

    started connection with openAI and added agents folder

--- src/agents/zero_shot.py
+++ src/agents/zero_shot.py
@@ -1,8 +1,21 @@
 from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 import os
+from agents.base_agent import BaseAgent
 
-api_key = os.environ.get('OPENAI_API_KEY')
-    if api_key is None:
-        raise ValueError("OPENAI_API_KEY environment variable is not set.")
+class ZeroShotAgent(BaseAgent):
+    llm = None
 
-llm = OpenAI(openai_api_key=api_key)
+    def __init__(self, llm):
+        self.llm = llm
+        prompt = PromptTemplate(
+            input_variables=["question"],
+            template="Please generate a corresponding SQL-query for this question: {question}?",
+        )
+        self.chain = LLMChain(llm=llm, prompt=prompt)
+
+    def generate_query(self, question):
+        return self.chain.run({
+            'question': question
+        })
--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 
 from db_interface import *
 from utils import load_json
+from langchain.chat_models import ChatOpenAI
 import os
+from agents.zero_shot import ZeroShotAgent
 
 QUESTIONS_PATH = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'data/questions.json'))
 
@@ -17,9 +19,12 @@ def main():
     if api_key is None:
         raise ValueError("OPENAI_API_KEY environment variable is not set.")
 
+    llm = ChatOpenAI(openai_api_key=api_key, model_name="gpt-3.5-turbo",temperature=0.9)
+
     questions = load_json(QUESTIONS_PATH)
     questions = [question for question in questions if question['db_id'] in ACCEPTED_DATABASES]
     db_loader = DBLoader(ACCEPTED_DATABASES)
+    zero_shot_agent = ZeroShotAgent(llm)
 
     score = 0
     total_questions = len(questions)
@@ -27,7 +32,12 @@ def main():
         if row['db_id'] in ACCEPTED_DATABASES:
             golden_sql = row['SQL']
             db_id = row['db_id']
-            
+            question = row['question']
+
+            predicted_sql = zero_shot_agent.generate_query(question)
+            print('Predicted query: ', predicted_sql)
+            break
+
             res = db_loader.execute_query(golden_sql, golden_sql, db_id)
             score += res
 
