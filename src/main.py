7e6ecfa9f23fea915e1b693e2218640edbb82d2c
Fredrik <fredrik@Admins-MacBook-Pro-2.local>
2023-09-26 09:44:56 +0200

    installed mlflow

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from data_interface import DataLoader
 from utils import load_json
 from langchain.chat_models import ChatOpenAI
 from agents.zero_shot import ZeroShotAgent
+import mlflow
 
 QUESTIONS_PATH = os.path.abspath(
     os.path.join(os.path.dirname( __file__ ), '..', 'data/questions.json'))
@@ -35,22 +36,26 @@ def main():
     zero_shot_agent = ZeroShotAgent(llm)
 
     score = 0
+    accuracy = None
     total_questions = len(questions)
-    for i, row in enumerate(questions):
-        if row['db_id'] in ACCEPTED_DATABASES:
-            golden_sql = row['SQL']
-            db_id = row['db_id']            
-            question = row['question']
-            
-            sql_schema = data_loader.get_create_statements(db_id)            
-            predicted_sql = zero_shot_agent.generate_query(sql_schema, question)            
+    with mlflow.start_run():
+        for i, row in enumerate(questions):
+            if row['db_id'] in ACCEPTED_DATABASES:
+                golden_sql = row['SQL']
+                db_id = row['db_id']            
+                question = row['question']
+                
+                sql_schema = data_loader.get_create_statements(db_id)            
+                predicted_sql = zero_shot_agent.generate_query(sql_schema, question)            
 
-            success = data_loader.execute_query(predicted_sql, golden_sql, db_id)
-            score += success
+                success = data_loader.execute_query(predicted_sql, golden_sql, db_id)
+                score += success
 
-            print("Percentage done: ", round(i / total_questions * 100, 2), "% Domain: ", db_id, " Success: ", success)
+                print("Percentage done: ", round(i / total_questions * 100, 2), "% Domain: ", db_id, " Success: ", success)
+        accuracy = score / len(questions)
+        mlflow.log_param("accuracy", accuracy)
 
-    print("accuracy: ", score / len(questions))
+        print("accuracy: ", accuracy)
 
-if __name__ == "__main__":
-    main()
+    if __name__ == "__main__":
+        main()
