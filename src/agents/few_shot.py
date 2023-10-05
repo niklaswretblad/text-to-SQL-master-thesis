bfd94416ff598e4fe101819e6b05606d4fb28b9b
Niklas Wretblad <wretbladniklas@gmail.com>
2023-10-05 16:08:23 +0200

    Few_shot now inherits from Zero_shot to avoid redundant code

--- src/agents/few_shot.py
+++ src/agents/few_shot.py
@@ -1,31 +1,40 @@
-from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
-import os
-from agents.base_agent import BaseAgent
+from agents.zero_shot import ZeroShotAgent
 
 FEW_SHOT_PROMPT = """
 Database schema in the form of CREATE_TABLE statements:
 {database_schema}
--- Using valid SQLite, answer the following questions for the tables provided above. 
+
+Here are a few examples, "Q"  represents the question and "A" represents the corresponding SQL-query :
+
+Q: Who is the top spending customer and how much is the average price per single item purchased by this customer? What currency was being used?
+A: SELECT T2.CustomerID, SUM(T2.Price / T2.Amount), T1.Currency FROM customers AS T1 INNER JOIN transactions_1k AS T2 ON T1.CustomerID = T2.CustomerID WHERE T2.CustomerID = ( SELECT CustomerID FROM yearmonth ORDER BY Consumption DESC LIMIT 1 ) GROUP BY T2.CustomerID, T1.Currency
+
+Q: What elements are in the TR004_8_9 bond atoms?
+A: SELECT DISTINCT T1.element FROM atom AS T1 INNER JOIN connected AS T2 ON T1.atom_id = T2.atom_id WHERE T2.bond_id = 'TR004_8_9'
+
+Q: What is the percentage of cards whose language is French among the Story Spotlight cards?
+A: SELECT CAST(SUM(CASE WHEN T2.language = 'French' THEN 1 ELSE 0 END) AS REAL) * 100 / COUNT(T1.id) FROM cards AS T1 INNER JOIN foreign_data AS T2 ON T1.uuid = T2.uuid WHERE T1.isStorySpotlight = 1
+
+Using valid SQL, answer the following question based on the tables provided above.
+It is important to use qualified column names in the SQL-query, meaning the form "SELECT table_name.column_name FROM table_name;"
+Hint helps you to write the correct sqlite SQL query.
 Question: {question}
-Example queries: 
-{examples}
-DO NOT return anything else except the SQL statement. 
+Hint: {evidence}
+DO NOT return anything else except the SQL query.
 """
 
-class FewShotAgent(BaseAgent):
+class FewShotAgent(ZeroShotAgent):
 
-    def __init__(self, llm):
+    def __init__(self, llm):        
         self.llm = llm
+
+        self.prompt_template = FEW_SHOT_PROMPT
         prompt = PromptTemplate(
-            input_variables=["question", "database_schema", "examples"],
-            template=FEW_SHOT_PROMPT,
+            input_variables=["question", "database_schema", "evidence"],
+            template=self.prompt_template,
         )
+
         self.chain = LLMChain(llm=llm, prompt=prompt)
 
-    def generate_query(self, database_schema, examples, question):
-        return self.chain.run({
-            'database_schema': database_schema,
-            'question': question            
-        })
--- src/agents/zero_shot.py
+++ src/agents/zero_shot.py
@@ -3,10 +3,20 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.callbacks import get_openai_callback
 from agents.base_agent import BaseAgent
-from config import config
 from utils.timer import Timer
 import logging
 
+ZERO_SHOT_PROMPT = """
+Database schema in the form of CREATE_TABLE statements:
+{database_schema}
+
+Using valid SQL, answer the following question based on the tables provided above.
+It is important to use qualified column names in the SQL-query, meaning the form "SELECT table_name.column_name FROM table_name;"
+Hint helps you to write the correct sqlite SQL query.
+Question: {question}
+Hint: {evidence}
+DO NOT return anything else except the SQL query.
+"""
 
 class ZeroShotAgent(BaseAgent):
     total_tokens = 0
@@ -16,12 +26,15 @@ class ZeroShotAgent(BaseAgent):
     last_call_execution_time = 0
     total_call_execution_time = 0
 
-    def __init__(self, llm):
+    def __init__(self, llm):        
         self.llm = llm
+
+        self.prompt_template = ZERO_SHOT_PROMPT
         prompt = PromptTemplate(    
             input_variables=["question", "database_schema","evidence"],
-            template=config.current_prompt,
+            template=ZERO_SHOT_PROMPT,
         )
+
         self.chain = LLMChain(llm=llm, prompt=prompt)
 
     def generate_query(self, database_schema, question, evidence):
